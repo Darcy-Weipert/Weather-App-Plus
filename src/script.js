3360bafd8baed46165a6ca1b070c1bb7e8380d89
Darcy-Weipert <144193277+Darcy-Weipert@users.noreply.github.com>
2023-09-08 19:21:43 -0500

    cleaned up javascript, adjusted  temp in css and updated windspeed

--- src/script.js
+++ src/script.js
@@ -35,7 +35,6 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
@@ -71,14 +70,6 @@ function getForecast(coordinates) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
-}
-
-function formatDay(timestamp) {
-  let date = new Date(timestamp * 1000);
-  let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-  return days[day];
 }
 
 function displayTemperature(response) {
